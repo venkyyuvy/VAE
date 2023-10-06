152e8936b17d86827a59e9bfc0e72b75e6336e10
Venkatachalam Natchiappan <venky.yuvy@gmail.com>
2023-10-06 18:05:19 +0530

    outputs

--- trainer.py
+++ trainer.py
@@ -10,10 +10,10 @@ if __name__ == "__main__":
     train_loader, test_loader = get_mnist_dataset()
     # vae = VAE.load_from_checkpoint(
     #     './lightning_logs/version_47/checkpoints/epoch=9-step=9380.ckpt')
-    vae = VAE.load_from_checkpoint(
-        './lightning_logs/version_86/checkpoints/epoch=1-step=1876.ckpt')
-
     # vae = VAE(x_dim=784, h_dim1=196, z_dim=z_dim)
+    vae = VAE.load_from_checkpoint(
+        './lightning_logs/version_87/checkpoints/epoch=14-step=14070.ckpt')
+
     trainer = pl.Trainer(
         max_epochs=20,
     )
